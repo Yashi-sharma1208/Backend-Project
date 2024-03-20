acc4e1a53d078a57d66136243a6f1b095b5be42a
Yashi <002yashi@gmail.com>
2024-03-20 19:33:32 +0530

    Worked on creating express app and add some configurations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import connectDB from "./db/connection.js";
+import app from "./app.js";
 dotenv.config({path:"./env"})
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(err)=>{
+        console.log("Failed to connect database with backend : "+err)
+        throw err;
+    })
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`App is running on port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Failed to connect with database : "+err)
+})
